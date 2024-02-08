c0ae5e1d4bc6dc44e386b1ed06212334f6aec0dd
TigerTechInc <tigertechprojects@gmail.com>
2024-02-08 13:17:54 -0500

    Updates

--- precache-manifest.b86eb3c985ca5c25d0735d6f7383e693.js
+++ precache-manifest.b86eb3c985ca5c25d0735d6f7383e693.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e33a1eb140e34c1aee411e722d99b5aa",
+    "revision": "44e2ad022603543318efd4903433ad00",
     "url": "./index.html"
   },
   {
-    "revision": "6d1bcddc4f339f63c0b8",
+    "revision": "5239990165ab5c773c38",
     "url": "./static/css/main.bb5878f9.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.9c41d402.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6d1bcddc4f339f63c0b8",
-    "url": "./static/js/main.dd7a197a.chunk.js"
+    "revision": "5239990165ab5c773c38",
+    "url": "./static/js/main.4e89f023.chunk.js"
   },
   {
     "revision": "a16126581cdad7f3a37c",
